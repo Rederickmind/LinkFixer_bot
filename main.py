c8d1331cb2b0fb0781d14778d57213b658db0df4
Mikhail Stralenia <mstralenya@gmail.com>
2023-11-17 20:43:11 +0300

    QoL changes: - Use re.sub - Send each found link in new message

--- main.py
+++ main.py
@@ -40,25 +40,21 @@ async def process_message(message: types.Message):
         r'(https?://(?:www\.)?(?:x|twitter|instagram)\.com/[^\s]+)',
         text
     )
-    modified_urls = []
     # Замена ссылок на новые значения
     for url in urls:
-        if 'x.com' in url:
-            modifiedurl = url.replace('x.com', 'fixupx.com')
-        elif 'twitter.com' in url:
-            modifiedurl = url.replace('twitter.com', 'vxtwitter.com')
-        elif 'instagram.com' in url:
-            modifiedurl = url.replace('instagram.com', 'ddinstagram.com')
-        modified_urls.append(modifiedurl)
-    if modified_urls:
-        if message.from_user.username:
-            modified_text = ("Saved @"
-                             + message.from_user.username
-                             + " a click:\n" + "\n".join(modified_urls))
-        else:
-            modified_text = ("Saved a click:\n" + "\n".join(modified_urls))
-        await message.reply(modified_text)
+        modifiedurl = replace(r'(x|twitter)\.com', 'fixupx.com', url)
+        modifiedurl = replace(r'instagram\.com', 'ddinstagram.com', modifiedurl)
+        if modifiedurl != url:
+            if message.from_user.username:
+                modified_text = ("Saved @"
+                                + message.from_user.username
+                                + " a click:\n" + "\n".join(modified_urls))
+            else:
+                modified_text = ("Saved a click:\n" + "\n".join(modified_urls))
+            await message.reply(modified_text)
 
+def replace(input, match, replacer)
+    return re.sub(match, replacer, input)
 
 async def main() -> None:
     await dp.start_polling(bot)
